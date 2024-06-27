6cdbcbda2d7f0e00fe8ccaffbb30a4e178d15845
kaungsike <kaungsike9@gmail.com>
2024-06-27 23:52:11 +0630

    learn php

---
+++ php-basic/operators.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = 5;
+$y = 4;
+
+// echo $x + $y;
+
+// echo $x . " kyats";
+echo $x == 5;
---
+++ php-basic/variable.php
@@ -0,0 +1,25 @@
+<?php
+
+
+// print_r($_SERVER);
+
+// define('MyName','Kyaw Kyaw');
+
+// const myName = "Kyaw Kyaw";
+
+// echo myName;
+
+// echo PHP_VERSION;
+
+
+// echo PHP_INT_SIZE;
+
+// echo __DIR__;
+
+// echo "\n";
+
+// echo __FILE__;
+
+// echo "\n";
+
+// echo __LINE__;
